5c3220964e4b998852d067a7a9811f8be8bcb836
Faruk Brbovic <fbrbovic@carqualifier.com>
2017-11-15 20:09:43 -0500

    testing returning regular value

--- dist/vuxtraController.js
+++ dist/vuxtraController.js
@@ -1,5 +1,5 @@
 /*!
- * Vuxtra.js v0.1.3
+ * Vuxtra.js v0.1.4
  * (c) 2017-2017 Faruk Brbovic
  * Released under the MIT License.
  */
@@ -9,151 +9,7 @@ function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'defau
 
 var socketclusterClient = _interopDefault(require('socketcluster-client'));
 var sharedCore = require('@vuxtra/shared-core');
-var _ = _interopDefault(require('lodash'));
-
-var asyncGenerator = function () {
-  function AwaitValue(value) {
-    this.value = value;
-  }
-
-  function AsyncGenerator(gen) {
-    var front, back;
-
-    function send(key, arg) {
-      return new Promise(function (resolve, reject) {
-        var request = {
-          key: key,
-          arg: arg,
-          resolve: resolve,
-          reject: reject,
-          next: null
-        };
-
-        if (back) {
-          back = back.next = request;
-        } else {
-          front = back = request;
-          resume(key, arg);
-        }
-      });
-    }
-
-    function resume(key, arg) {
-      try {
-        var result = gen[key](arg);
-        var value = result.value;
-
-        if (value instanceof AwaitValue) {
-          Promise.resolve(value.value).then(function (arg) {
-            resume("next", arg);
-          }, function (arg) {
-            resume("throw", arg);
-          });
-        } else {
-          settle(result.done ? "return" : "normal", result.value);
-        }
-      } catch (err) {
-        settle("throw", err);
-      }
-    }
-
-    function settle(type, value) {
-      switch (type) {
-        case "return":
-          front.resolve({
-            value: value,
-            done: true
-          });
-          break;
-
-        case "throw":
-          front.reject(value);
-          break;
-
-        default:
-          front.resolve({
-            value: value,
-            done: false
-          });
-          break;
-      }
-
-      front = front.next;
-
-      if (front) {
-        resume(front.key, front.arg);
-      } else {
-        back = null;
-      }
-    }
-
-    this._invoke = send;
-
-    if (typeof gen.return !== "function") {
-      this.return = undefined;
-    }
-  }
-
-  if (typeof Symbol === "function" && Symbol.asyncIterator) {
-    AsyncGenerator.prototype[Symbol.asyncIterator] = function () {
-      return this;
-    };
-  }
-
-  AsyncGenerator.prototype.next = function (arg) {
-    return this._invoke("next", arg);
-  };
-
-  AsyncGenerator.prototype.throw = function (arg) {
-    return this._invoke("throw", arg);
-  };
-
-  AsyncGenerator.prototype.return = function (arg) {
-    return this._invoke("return", arg);
-  };
-
-  return {
-    wrap: function (fn) {
-      return function () {
-        return new AsyncGenerator(fn.apply(this, arguments));
-      };
-    },
-    await: function (value) {
-      return new AwaitValue(value);
-    }
-  };
-}();
-
-
-
-var asyncToGenerator = function (fn) {
-  return function () {
-    var gen = fn.apply(this, arguments);
-    return new Promise(function (resolve, reject) {
-      function step(key, arg) {
-        try {
-          var info = gen[key](arg);
-          var value = info.value;
-        } catch (error) {
-          reject(error);
-          return;
-        }
-
-        if (info.done) {
-          resolve(value);
-        } else {
-          return Promise.resolve(value).then(function (value) {
-            step("next", value);
-          }, function (err) {
-            step("throw", err);
-          });
-        }
-      }
-
-      return step("next");
-    });
-  };
-};
+var lodash = _interopDefault(require('lodash'));
 
 let privateData = new WeakMap();
 
@@ -172,9 +28,29 @@ class VuxtraController {
             this._internalSocketConnected = true;
         });
 
-        var $_internalService = (callArguments, options, action) => {
+        let $_doBindOrExecute = func => {
+            if (this._internalSocketConnected !== true) {
+                this.socket.on('connect', function () {
+                    func();
+                });
+            } else {
+                func();
+            }
+        };
+
+        let $_getVarFromData = response => {
+            let res = response.getData();
+
+            res.prototype.getResponse = function () {
+                return response;
+            };
+
+            return res;
+        };
+
+        let $_internalService = (callArguments, options, action) => {
             return new Promise((resolve, reject) => {
-                this.doBindOrExecute(() => {
+                $_doBindOrExecute(() => {
                     let request = new sharedCore.ServiceRequest();
                     request.setServiceName(options).setServiceAction(action).setData(callArguments).setId(this.socket.id + this._internalRequestCounter);
                     this._internalRequestCounter++;
@@ -183,7 +59,7 @@ class VuxtraController {
                             reject(err);
                         } else {
                             let response = new sharedCore.ServiceResponse(res);
-                            resolve(response);
+                            resolve($_getVarFromData(response));
                         }
                     });
                 });
@@ -193,45 +69,6 @@ class VuxtraController {
         this.services = require('~/.vuxtra/clientVuxtra.js').services($_internalService, this);
     }
 
-    doBindOrExecute(func) {
-        if (this._internalSocketConnected !== true) {
-            this.socket.on('connect', function () {
-                func();
-            });
-        } else {
-            func();
-        }
-    }
-
-    /**
-     * Makes a realtime service call to the backend via socket
-     *
-     * @param requestFunc callback function / closure which gets ServiceRequest Object as it's first parameter (serviceRequest) => { serviceRequest.setData('hello') }
-     * @param data  payload to be passed to service method
-     * @returns {Promise}
-     */
-    call(requestFunc) {
-        var _this = this;
-
-        return asyncToGenerator(function* () {
-
-            if (!_.isFunction(requestFunc)) {
-                throw Error('requestFunc [ first param ] must of function tyupe');
-            }
-            let request = new sharedCore.ServiceRequest();
-            requestFunc(request);
-            return new Promise(function (resolve, reject) {
-                _this.socket.emit('service.call', request, function (err, res) {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        let response = new sharedCore.ServiceResponse(res);
-                        resolve(response);
-                    }
-                });
-            });
-        })();
-    }
 }
 
 module.exports = VuxtraController;
